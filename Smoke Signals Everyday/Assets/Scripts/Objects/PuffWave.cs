cbc0b5bd29433261216aa012ace1c6ebfd4a0922
Matias Pitkänen <M2tias@GMail.com>
2017-01-21 12:52:02 +0200

    :dash: Changes to PuffWave.

--- Smoke Signals Everyday/Assets/Scripts/Objects/PuffWave.cs
+++ Smoke Signals Everyday/Assets/Scripts/Objects/PuffWave.cs
@@ -2,12 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PuffWave : MonoBehaviour {
+public class PuffWave : MonoBehaviour
+{
+
+    [SerializeField]
+    private Rigidbody2D rb2d;
+
+    [SerializeField]
+    [Range(0.2f, 2f)]
+    private float horizontalSpeed = 1f;
 
     [SerializeField]
     public Puff Puff;
     [SerializeField]
     public Wave Wave;
+
+    [SerializeField]
+    private bool isMoving = false;
 
 	// Use this for initialization
 	void Start () {
@@ -15,7 +26,11 @@ public class PuffWave : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        if (isMoving)
+        {
+            rb2d.velocity = new Vector3(horizontalSpeed, rb2d.velocity.y, 0f);
+        }
+    }
 }
