d549169c06e8ef4e901680cf961ee928a2091edf
Juho Saarelainen <bradurdung@gmail.com>
2017-01-21 13:05:45 +0200

    :newspaper: Puff spriterenderer.

--- Smoke Signals Everyday/Assets/Scripts/Objects/Puff.cs
+++ Smoke Signals Everyday/Assets/Scripts/Objects/Puff.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 [RequireComponent(typeof(SpriteRenderer))]
 public class Puff : MonoBehaviour {
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
+    
+    public Sprite Sprite { get { return spriteRenderer.sprite; } set { spriteRenderer.sprite = value; } }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void SetSprite(Sprite newSprite)
+    {
+        spriteRenderer.sprite = newSprite;
+    }
+
+    // Use this for initialization
+    void Start () {
     }    
     // Update is called once per frame
     void Update () {
